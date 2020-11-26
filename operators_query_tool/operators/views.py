1f58ec96fb56d3bd8e6faa381f3cb1748fdb8f73
David O'Neill <2378524o@student.gla.ac.uk>
2020-11-26 12:15:15 +0000

    Finished the basic styling for Operators Web App
    Added styling for the basic pages
    Wait until work has been done on the models and user authentication
    before more can be done.

--- operators_query_tool/operators/views.py
+++ operators_query_tool/operators/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+from operators.forms import operatorQueryForm
+
+
 
 # Create your views here.
 
@@ -16,10 +21,34 @@ def myaccount(request):
     return render(request, 'operators/myaccount.html')
 
 def querydata(request):
-    return render(request, 'operators/querydata.html')
+    form = operatorQueryForm()
+
+    if request.method=="POST":
+        form = operatorQueryForm(request.POST)
+
+        if form.is_valid():
+            query_type = form.cleaned_data.get('query_type')
+            if query_type=='mode':
+                return redirect(reverse('operators:viewmodes'))
+            elif query_type=='operator':
+                #redirect to the page showing operators info
+                #need to understand what is going on before we can implement this
+                return redirect(reverse('operators:viewoperators'))
+
+
+            
+    context = {'form':form}
+    
+    return render(request, 'operators/querydata.html', context=context )
 
 def changedata(request):
     return render(request, 'operators/changedata.html')
 
-def viewdata(request):
-    return render(request, 'operators/viewdata.html')
+def viewmodes(request):
+    #query the operators API with the get request /mode
+    #the context dictionary will contain the data we have returned from the query
+
+    return render(request, 'operators/viewmodes.html')
+
+def viewoperators(request):
+    return render(request, 'operators/viewOperators.html')
