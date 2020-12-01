47aeddd2c960006a637e3d557f9c2a2e2be5e789
Ans Farooq <2390370f@student.gla.ac.uk>
2020-12-01 15:10:07 +0000

    Back end conflict

--- operators_query_tool/operators_query_tool/settings.py
+++ operators_query_tool/operators_query_tool/settings.py
@@ -12,15 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Think we might need media dir so that people can change profile picture
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -32,15 +29,13 @@ SECRET_KEY = '!rcw887i(6_07gqdl6^*&b0z$=un!8gwq$8pqyn#5w@q5*w%$0'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-                
-                ]
 
-
+]
 
 # Application definition
 
 INSTALLED_APPS = [
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,11 +56,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'operators_query_tool.urls'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'operators_query_tool.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -91,7 +86,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -111,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -124,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -142,5 +134,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # adding MEDIA_DIR
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-
-
