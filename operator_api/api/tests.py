192cfc1cf8e75c43940d8941353752951b3b58ee
Pragati Mishra <2506109m@student.gla.ac.uk>
2021-01-18 01:44:44 +0000

    testapi

--- operator_api/api/tests.py
+++ operator_api/api/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.contrib.auth import get_user_model
+
+
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APIRequestFactory
+
+from api import views
+
+
+class TestPoll(APITestCase):
+    def setUpa(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = self.setup_user()
+        self.view =views.ModeViewSet.as_view({'post': 'list'})
+
+
+    def setUpb(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = self.setup_user()
+        self.view =views.CatalogueViewSet.as_view({'post': 'list'})
+
+    def setUpc(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = self.setup_user()
+        self.view =views.ItemViewSet.as_view({'post': 'list'})
