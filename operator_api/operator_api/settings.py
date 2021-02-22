1a42093b9f0b5bdac10090fcdc3f14ae34cf19fd
Ans <2390370f@student.gla.ac.uk>
2021-02-22 22:52:02 +0000

    Minor bug fix
    * Replaced 'rest_framework.authtoken' line that I accidentally took out in the previous commit

--- operator_api/operator_api/settings.py
+++ operator_api/operator_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['cs21operatorapi.pythonanywhere.com', '127.0.0.1', ]
 INSTALLED_APPS = [
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
